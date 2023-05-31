ae9ef8a9953ec8af25a9aa9df43ce4405a7f37e0
Petar <petar.stika12345@gmail.com>
2023-06-01 01:02:27 +0200

    Add installation process

---
+++ install.bat
@@ -0,0 +1,11 @@
+@echo off
+for /f "delims=" %%x in ('powershell -Command "((Get-Content -Path config.json -Raw | ConvertFrom-Json).desiredPath)"') do set desiredPath=%%x
+for /f "delims=" %%x in ('powershell -Command "((Get-Content -Path config.json -Raw | ConvertFrom-Json).commandName)"') do set commandName=%%x
+setx PATH "%PATH%;%desiredPath%" /M
+
+cd %~dp0
+set currentPath=%CD%
+set mainPyPath=%currentPath%\src\main.py
+
+echo @echo off > %desiredPath%\%commandName%.bat
+echo python "%mainPyPath%" %%* >> %desiredPath%\%commandName%.bat
