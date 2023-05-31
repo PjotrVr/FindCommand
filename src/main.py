964d7fa6f9b668b54a03edf86b7e40da16ce1786
Petar <petar.stika12345@gmail.com>
2023-06-01 00:16:43 +0200

    Displays custom help message and banner

--- src/main.py
+++ src/main.py
@@ -1,13 +1,12 @@
 import os
 import argparse
 
+from sys import exit
+from art import text2art
 from search import search_dir, search_file, search_dirfile
 from search import search_functions
 
-with open("../help.txt") as f:
-    help_text = f.read()
-    
-parser = argparse.ArgumentParser(description=help_text)
+parser = argparse.ArgumentParser(add_help=False)
 
 # arguments for searching
 group_search = parser.add_mutually_exclusive_group(required=False)
@@ -20,8 +19,17 @@ parser.add_argument("-cs", "--case_sensitive", action="store_true", help="Is cas
 parser.add_argument("--name", default="", type=str, help="Name pattern for files and directories")
 parser.add_argument("start_path", type=str, help="Starting path of the search")
 
-# parse args
-args = parser.parse_args()
+# parsing args, if illegal flag occours then help message printed
+try:
+    args = parser.parse_args()
+
+except SystemExit:
+    with open("../help.txt") as f:
+        help_text = f.read()
+
+    print(text2art("FindCommand", font="small"))
+    print(help_text)
+    exit(1)
 
 if args.dir:
     search_type = "dir"
@@ -31,6 +39,6 @@ elif args.file:
     
 else:
     search_type = "dirfile"
-
+    
 search_function = search_functions[search_type]
 search_function(args.start_path, args.name, args.case_sensitive)
