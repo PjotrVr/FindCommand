cc069912e2b6b6f28bb14acc37024512866caef1
Petar <petar.stika12345@gmail.com>
2023-05-31 11:39:14 +0200

    Search highlights regex in messages

--- src/search.py
+++ src/search.py
@@ -1,20 +1,27 @@
 import os
 import re
-
+from highlight import highlight_terminal
 
 def search_file(start_path, name):
     for root, dirs, files in os.walk(start_path):
         for file in files:
-            if re.match(name, file):
-                print(os.path.normpath(os.path.join(root, file)))
+            full_path = os.path.normpath(os.path.join(root, file))
+            match = re.search(name, file)
+            if match:
+                start_index = match.start() + len(root) + 1 # added + 1 so it doesn't include slashes
+                end_index = match.end() + len(root)
+                highlight_terminal(full_path, start_index, end_index)
 
-                
+
 def search_dir(start_path, name):
     for root, dirs, _ in os.walk(start_path):
         for directory in dirs:
-            if re.match(name, directory):
-                print(os.path.normpath(os.path.join(root, directory)))
-              
+            full_path = os.path.normpath(os.path.join(root, directory))
+            match = re.search(name, directory)
+            if match:
+                start_index = match.start() + len(root) + 1 # added + 1 so it doesn't include slashes
+                end_index = match.end() + len(root)
+                highlight_terminal(full_path, start_index, end_index)             
   
 def search_dirfile(start_path, name):
     search_file(start_path, name)
@@ -29,3 +36,4 @@ search_functions = {
 
 if __name__ == "__main__":
     search_dir("D:/", "Karpathy")
+    #search_file("D:/Programming", "main.*")
--- src/test.py
+++ src/test.py
@@ -1,9 +1,13 @@
 import re
+from sys import exit
 
 text = "My name is Peter and his name is Paul"
 pattern = "is"
 matches = re.finditer(pattern, text)
-
+a = next(matches)
+print(type(a))
+print(a.start())
+exit(0)
 for match in matches:
     #print("Match found at start index:", match.start())
     #print("Match found at end index:", match.end())
