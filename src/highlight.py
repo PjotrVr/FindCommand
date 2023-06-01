fc2924a290c3ab3c92368827af19e026dc95be6c
Petar <petar.stika12345@gmail.com>
2023-06-01 09:53:12 +0200

    Remove background color

--- src/highlight.py
+++ src/highlight.py
@@ -2,9 +2,9 @@ from colorama import Fore, Back, Style, init
 
 init(autoreset=True)
 
-def highlight_terminal(message, start_index, end_index, color_text=Fore.RED, color_back=Back.BLACK):
+def highlight_terminal(message, start_index, end_index, color_text=Fore.RED):
     print(message[:start_index], end="")
-    print(color_text + color_back + message[start_index:end_index + 1] + Style.RESET_ALL, end="")
+    print(color_text + message[start_index:end_index + 1] + Style.RESET_ALL, end="")
     print(message[end_index + 1:])
 
 if __name__ == "__main__":
